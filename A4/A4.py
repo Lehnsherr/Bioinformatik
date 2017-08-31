f62c4382802edbd3e7fbb20b5b04d2fed53dd91d
j sey <schnorrisnorris@gmail.com>
2017-08-31 21:35:09 +0200

    Clean up code, small bug fixes

--- A4/A4.py
+++ A4/A4.py
@@ -5,28 +5,37 @@
 
 
 import json
-import Tkinter as tk
 import matplotlib.pyplot as plt
-import matplotlib as mpl
 import numpy as np
 import argparse
 
-faktor = 1
 
 # #--##--##--##--##--##--##--##--##--##--##--##--##--##--##--##--##--##--##--##
 # Viterbi Algorithmus
 # #--##--##--##--##--##--##--##--##--##--##--##--##--##--##--##--##--##--##--##
 
 
-def viterbi(sequenz, zustaende, zustaende_wsl, uebergangs_wsl, emmisions_wsl):
+def viterbi(sequenz, zustaende, zustaende_wsl, uebergangs_wsl, emmisions_wsl, reverse_loop):
+    print 'sequenz :'
+    print sequenz
+    print 'Zustaende :'
+    print zustaende
+    print 'StartWsl :'
+    print zustaende_wsl
+    print 'UebergangsWsl :'
+    print uebergangs_wsl
+    print 'EmmisionsWsl :'
+    print emmisions_wsl
+
     viterbi_list = [{}]
     opt_wsl = []
-    opt_wsl_str = ""
     j = 0
-    max_wsl = 0
 
     for zustand in zustaende:
-        viterbi_list[0][zustand] = {"Wahrscheinlichkeit": np.float128((zustaende_wsl[zustand] * emmisions_wsl[zustand][sequenz[0]])*faktor), "vorher": None, "Index": j}
+        viterbi_list[0][zustand] = {"Wahrscheinlichkeit": np.float128(zustaende_wsl[zustand] * emmisions_wsl[zustand][sequenz[0]]), "vorher": None, "Index": j}
+
+    if(reverse_loop):
+        zustaende = list(reversed(zustaende))
 
     for i in range(1, len(sequenz)):
 
@@ -39,14 +48,11 @@ def viterbi(sequenz, zustaende, zustaende_wsl, uebergangs_wsl, emmisions_wsl):
 
             for vorher_zustand in zustaende:
                 if viterbi_list[i-1][vorher_zustand]["Wahrscheinlichkeit"] * uebergangs_wsl[vorher_zustand][zustand] == max_uebergangs_wsl:
-                    
                     max_wsl = np.float128(max_uebergangs_wsl * emmisions_wsl[zustand][sequenz[i]])
+
                     viterbi_list[i][zustand] = {"Wahrscheinlichkeit": max_wsl, "vorher": vorher_zustand, "Index": i}
 
                     break
-
-    # for line in printViterbiList(viterbi_list):
-    #    print line
 
     # Hoehste Wahrscheinlichkeit berechnen
     max_wsl = max(value["Wahrscheinlichkeit"] for value in viterbi_list[-1].values())
@@ -63,26 +69,24 @@ def viterbi(sequenz, zustaende, zustaende_wsl, uebergangs_wsl, emmisions_wsl):
             break
 
     # Pfad zurueck verfolgen bis zur ersten Begegnung
-    for i in range(len(viterbi_list) - 2, -1, -1):
-        opt_wsl.insert(0, viterbi_list[i + 1][previous]["vorher"])
-        previous = viterbi_list[i + 1][previous]["vorher"]
+    for k in range(len(viterbi_list) - 2, -1, -1):
+        opt_wsl.insert(0, viterbi_list[k + 1][previous]["vorher"])
+        previous = viterbi_list[k + 1][previous]["vorher"]
 
-    opt_wsl_str = toStringMaxWsl(viterbi_list, previous)
+    print 'Viterbi-List :'
+    for line in viterbi_list:
+        print str(line)
 
-    # print ' \n Die Abfolge der Zustaende ist \n '+' '.join(opt_wsl) + '\n mit der hoehsten Wahrscheinlichkeit von %s' % max_wsl + '\n'
-    print ' \n Die Abfolge der Zustaende ist \n ' + opt_wsl_str + '\n mit der hoehsten Wahrscheinlichkeit von %s' % max_wsl + '\n'
-
-    # for line in viterbi_list:
-    #     print str(line) + '\n'
+    print ' \n Die Abfolge der Zustaende ist \n '+' '.join(opt_wsl) + '\n mit der hoehsten Wahrscheinlichkeit von %s' % max_wsl + '\n'
 
     return viterbi_list
 
-
+"""
 def printViterbiList(viterbi_list):
+    print 'Viterbi-List :'
     for zustand in viterbi_list[0]:
-        # yield "%.20s: " % zustand + " ".join("%.20s" % ("%f" % viterbi_item[zustand]["Wahrscheinlichkeit"]) for viterbi_item in viterbi_list)
-        yield zustand + " ".join('\n' + str(viterbi_item[zustand]["Index"]) + ' ' + str(viterbi_item[zustand]["Wahrscheinlichkeit"]) for viterbi_item in viterbi_list)
-
+        print zustand + " ".join('\n' + str(viterbi_item[zustand]["Index"]) + ' ' + str(viterbi_item[zustand]["Wahrscheinlichkeit"]) for viterbi_item in viterbi_list)
+"""
 
 # #--##--##--##--##--##--##--##--##--##--##--##--##--##--##--##--##--##--##--##
 # Forward 
@@ -90,6 +94,17 @@ def printViterbiList(viterbi_list):
 
 
 def forward(sequenz, zustaende, zustaende_wsl, uebergangs_wsl, emmisions_wsl):
+    print 'sequenz :'
+    print sequenz
+    print 'Zustaende :'
+    print zustaende
+    print 'StartWsl :'
+    print zustaende_wsl
+    print 'UebergangsWsl :'
+    print uebergangs_wsl
+    print 'EmmisionsWsl :'
+    print emmisions_wsl
+
     forward_list = []
     forward_vorher = {}
     for i, observation_i in enumerate(sequenz):
@@ -102,71 +117,21 @@ def forward(sequenz, zustaende, zustaende_wsl, uebergangs_wsl, emmisions_wsl):
                 forward_vorher_sum = sum(forward_vorher[k]*uebergangs_wsl[k][zustand] for k in zustaende)
 
             forward_current[zustand] = emmisions_wsl[zustand][observation_i] * forward_vorher_sum
-
+        
         forward_list.append(forward_current)
         forward_vorher = forward_current
 
-    # p_fwd = sum(forward_current[k] * uebergangs_wsl[k][end_st] for k in zustaende)
-
-    # return p_fwd, fwd
-    print 'Vorwardlist'
+    print 'Vorwaerts-List :'
     for forward_item in forward_list:
-        print str(forward_list.index(forward_item)) + ':-->' + str(forward_item) + '\n'
+        print str(forward_list.index(forward_item)) + ':-->' + str(forward_item)
 
     return forward_list
 
-# Wird nicht benoetigt
 """
-def backward(sequenz, zustaende, zustaende_wsl, uebergangs_wsl, emmisions_wsl):
-    # backward part of the algorithm
-    backward_list = []
-    backward_vorher = {}
-    for i, observation_i_plus in enumerate(reversed(sequenz[1:]+(None,))):
-        backward_current = {}
-        for zustand in zustaende:
-            if i == 0:
-                # base case for backward part
-                backward_current[zustand] = uebergangs_wsl[zustand][end_st]
-            else:
-                backward_current[zustand] = sum(uebergangs_wsl[zustand][l] * emmisions_wsl[l][observation_i_plus] * backward_vorher[l] for l in zustaende)
-
-        backward_list.insert(0,backward_current)
-        backward_vorher = backward_current
-
-    p_bkw = sum(zustaende_wsl[l] * emmisions_wsl[l][sequenz[0]] * backward_current[l] for l in zustaende)
-
-    return p_bkw, backward_list
-
-
-def fwd_bkw(sequenz, zustaende, end_st, p_bkw, p_fwd):
-    # merging the two parts
-    posterior = []
-    for i in range(len(sequenz)):
-        posterior.append({zustand: forward_list[i][zustand] * bkw[i][zustand] / p_fwd for zustand in zustaende})
-
-    # assert p_fwd == p_bkw
-    return forward_list, bkw, posterior
-"""
-
-
-def toStringMaxWsl(viterbi_list, previous):
-    opt_to_string = ''
-    for i in range(len(viterbi_list) - 2, -1, -1):
-        previous = viterbi_list[i + 1][previous]["vorher"]
-
-        if previous == 'ungezinkt':
-            previous_str = 'F'
-        elif previous == 'gezinkt':
-            previous_str = 'L'
-
-        opt_to_string = opt_to_string + previous_str
-
-    return opt_to_string
-
-
 def toStringTest(viterbi_list):
     for zustand in viterbi_list[0]:
             yield "%.20s: " % zustand + " ".join("%.20s" % ("%f" % viterbi_item[zustand]["Wahrscheinlichkeit"]) for viterbi_item in viterbi_list)
+"""
 
 
 def readTxtFile(file_name):
@@ -185,7 +150,7 @@ def readJsonFile(file_name):
     return j_data
 
 
-def getSequenz(result_data, name):
+def getSequenz(result_data, name, reverse):
     arr = []
     test_str = ''
     for line in result_data:
@@ -193,9 +158,11 @@ def getSequenz(result_data, name):
 
         if (arr[0] == name):
             # Expertiment Sequenz herumdrehen 
-            # return list(reversed_string(str(arr[1])))
-            return list(arr[1])
-
+            if(reverse): 
+                return list(reversed_string(str(arr[1])))
+            else:
+                return list(arr[1])
+           
 
 def getZustaende(arr):
     zstd_arr = []
@@ -220,11 +187,11 @@ def buildViterbiScala(viterbi_list, plot):
     x_2_arr = []
 
     for viterbi_item in viterbi_list:
-        y_1_arr.append(viterbi_item['ungezinkt']['Wahrscheinlichkeit'])
-        x_1_arr.append(viterbi_item['ungezinkt']['Index'])
+        y_1_arr.append(viterbi_item['F']['Wahrscheinlichkeit'])
+        x_1_arr.append(viterbi_item['F']['Index'])
 
-        y_2_arr.append(viterbi_item['gezinkt']['Wahrscheinlichkeit'])
-        x_2_arr.append(viterbi_item['gezinkt']['Index'])
+        y_2_arr.append(viterbi_item['L']['Wahrscheinlichkeit'])
+        x_2_arr.append(viterbi_item['L']['Index'])
 
     fig, ax = plt.subplots()
 
@@ -248,10 +215,10 @@ def buildForwardScala(forward_list, plot):
     x_2_arr = []
 
     for forward_item in forward_list:
-        y_1_arr.append(forward_item['ungezinkt'])
+        y_1_arr.append(forward_item['F'])
         x_1_arr.append(forward_list.index(forward_item))
 
-        y_2_arr.append(forward_item['gezinkt'])
+        y_2_arr.append(forward_item['L'])
         x_2_arr.append(forward_list.index(forward_item))
 
     fig, ax = plt.subplots()
@@ -278,20 +245,47 @@ __parser__ = argparse.ArgumentParser(
                 "Example: Viterbi Algorithm + Plot Log : python A4_2.py -a vit -p l")
 
 __parser__.add_argument(
+    "-f",
+    "--file",
+    type=str,
+    required=True,
+    help="Text Datei mit Zahlenfolge \n" + 
+         "Bsp.: -f wuerfel.txt")
+
+__parser__.add_argument(
     "-a",
     "--algorithmus",
     type=str,
     required=True,
-    help=" Viterbi-Alg == '"'vit'"' or '"'Viterbi'"'. \n " +
-        " Vorwaerts-Alg == '"'vor'"' or '"'Vorwaerts'"'. \n " +
-        " Rueckwaerts-Alg == '"'ruck'"' or '"'Rueckwarts'"'. ")
+    help=" Viterbi-Alg == '"'vit'"' oder '"'Viterbi'"'. \n " +
+         " Vorwaerts-Alg == '"'vor'"' oder '"'Vorwaerts'"'. \n " + 
+         "Bsp.: -a vit  oder -a vor")
 
 __parser__.add_argument(
     "-p",
     "--plot",
     type=str,
     required=False,
-    help="l(= logarithmische Skala) || n (= normale Skala)")
+    help="Soll eine Skale erstellt werden ? \n" + 
+         "(l = logarithmische Skala) ||p (p= normale Skala)"
+         "Bsp.: -p l  oder -p n")
+
+__parser__.add_argument(
+    "-rs",
+    "--reverse_Sequenz",
+    type=str,
+    required=False,
+    help="Soll die Reihenfolge der Zeichen der Eingabesequenz vertauscht werden (y/n)? \n" + 
+         "Bsp.: -rs y  oder -rs n")
+
+__parser__.add_argument(
+    "-rl",
+    "--reverse_Loop",
+    type=str,
+    required=False,
+    help="Soll Reihenfolge der Zustaende vertauscht werden (y/n)? \n" + 
+         "Bsp.: -rl y oder -rl n")
+
 
 # Parsen der Kommandozeilen-Args
 if __name__ == '__main__':
@@ -300,30 +294,31 @@ if __name__ == '__main__':
     __algorithmus__ = __args__.algorithmus
     __plot__ = __args__.plot
 
-    __result_data__ = readTxtFile('wuerfel.txt')
-
+    __result_data__ = readTxtFile(__args__.file)
     __wsl__ = readJsonFile('wsl_wuerfel.json')
 
     __zustaende_wsl__ = __wsl__['Zustaende']
-    __uebergangs_wsl__ = __wsl__['Uebergangs']
-    __emmisions_wsl__ = __wsl__['Emmisionswsl']
+    __uebergangs_wsl__ = __wsl__['UebergangsWsl']
+    __emmisions_wsl__ = __wsl__['EmmisionsWsl']
 
-    __sequenz__ = getSequenz(__result_data__, 'zahlenfolge')
+    if __args__.reverse_Sequenz == 'y':
+        __rs__ = True
+    else:
+        __rs__ = False
+
+    if __args__.reverse_Loop == 'y':
+        __rl__ = True
+    else:
+        __rl__ = False
+
+    __sequenz__ = getSequenz(__result_data__, 'zahlenfolge', __rs__)
     __zustaende__ = getZustaende(__zustaende_wsl__)
 
-    print(__sequenz__)
-    print(__zustaende__)
-    print(__zustaende_wsl__)
-    print(__uebergangs_wsl__)
-    print(__emmisions_wsl__)
-
     if __algorithmus__ == 'vit' or __algorithmus__ == 'Viterbi':
-        viterbi_list = viterbi(__sequenz__, __zustaende__, __zustaende_wsl__, __uebergangs_wsl__, __emmisions_wsl__)
+        viterbi_list = viterbi(__sequenz__, __zustaende__, __zustaende_wsl__, __uebergangs_wsl__, __emmisions_wsl__, __rl__)
         buildViterbiScala(viterbi_list, __plot__)
     elif __algorithmus__ == 'vor' or __algorithmus__ == 'Vorwaerts':
         forward_list = forward(__sequenz__, __zustaende__, __zustaende_wsl__, __uebergangs_wsl__, __emmisions_wsl__)
         buildForwardScala(forward_list, __plot__)
-    elif __algorithmus__ == 'ruck' or __algorithmus__ == 'Rueckwarts':
-        print'Backward Algorithmus wurde nicht implementiert, da nur einer benoetigt'
     else:
-        print 'BUG'
+        print 'Err: algorithm not found'
--- A4/README.md
+++ A4/README.md
@@ -2,16 +2,36 @@
 
 Viterbi / Forwards Algorithmus mit Skala plot.
 
-
 #Benutztung: 
-  A4_2.py [-h] -a ALGORITHMUS [-p PLOT]
+ Paramter in [] sind optional. 
+
+python A4.py  [-h]
+              -f FILE 
+              -a ALGORITHMUS 
+              [-p PLOT] 
+              [-rs REVERSE_SEQUENZ] 
+              [-rl REVERSE_LOOP]
 
 #Beispiel: 
-  Viterbi Algorithm + Plot Log : python A4_2.py -a vit -p l
+  Viterbi Algorithm + Plot Logaritmisch : 
+  python A4.py -f wuerfel.txt -a vit -p l
+
+  Vorwärts Alotithmus + Plot normal + Umdrehen Sequenz:
+  python A4.py -f wuerfel.txt -a vor -p n -rs y
 
 #optionale Argumente:
   -h, --help            zeige Hilfe Nachricht, Exit
+  -f FILE, --file FILE  Text Datei mit Zahlenfolge Bsp.: -f wuerfel.txt  
   -a ALGORITHMUS, --algorithmus ALGORITHMUS
                         Viterbi-Alg == 'vit' oder 'Viterbi'. 
                         Vorwaerts-Alg == 'vor' oder 'Vorwaerts'. 
-  -p PLOT, --plot PLOT  l(= logarithmische Skala) || n (= normale Skala)
+                        Bsp.: -a vit oder -a vor
+  -p PLOT, --plot PLOT  Soll eine Skale erstellt werden ? 
+                        (l = logarithmische Skala) ||p (p= normale Skala)
+                        Bsp.: -p l oder -p n
+  -rs REVERSE_SEQUENZ, --reverse_Sequenz REVERSE_SEQUENZ
+                        Soll die Reihenfolge der Zeichen der Eingabesequenz vertauscht werden (y/n)? 
+                        Bsp.: -rs y oder -rs n
+  -rl REVERSE_LOOP, --reverse_Loop REVERSE_LOOP
+                        Soll Reihenfolge der Zustaende vertauscht werden (y/n)? 
+                        Bsp.: -rl y oder -rl n
